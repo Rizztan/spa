bc942a40770e2cf48d76b6895ae2361730eb4a21
Rizztan <kristan.martinito@urios.edu.ph>
2025-03-27 03:30:18 +0000

    first commit

---
+++ script/app.js
@@ -0,0 +1,14 @@
+
+
+Alpine.store("settings", {
+    apiBaseUrl:"http://172.17.100.14:3332/martinito3/api",  
+    appName: "contact application",
+    
+})
+    
+Alpine.store("settings", {
+    apiBaseUrl:"http://172.17.100.14:3332/martinito3/api/exam",  
+    appName: "chat application",  
+    
+   
+});
